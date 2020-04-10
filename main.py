db998bc1f77157000b3a156b2b9ff04fc19ab2eb
Vo Ho Viet Khoa <vhvkhoa@gmail.com>
2020-04-10 12:14:35 +0900

    init

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import json
 import torch
 import torch.nn.parallel
 import torch.optim as optim
+from torch.utils.tensorboard import SummaryWriter
 import numpy as np
 import opts
 from models import BMN
@@ -16,12 +17,13 @@ from eval import evaluation_proposal
 sys.dont_write_bytecode = True
 
 
-def train_BMN(data_loader, model, optimizer, epoch, bm_mask):
+def train_BMN(data_loader, model, optimizer, epoch, bm_mask, writer):
     model.train()
     epoch_pemreg_loss = 0
     epoch_pemclr_loss = 0
     epoch_tem_loss = 0
     epoch_loss = 0
+    data_sz = len(data_loader)
     for n_iter, (input_data, label_confidence, label_start, label_end) in enumerate(data_loader):
         input_data = input_data.cuda()
         label_start = label_start.cuda()
@@ -32,6 +34,17 @@ def train_BMN(data_loader, model, optimizer, epoch, bm_mask):
         optimizer.zero_grad()
         loss[0].backward()
         optimizer.step()
+        # print('Iter: %d\tLoss: %f\tTem: %f\tPemclr: %f\tPemreg: %f' % (
+        #     n_iter,
+        #     loss[0].item(),
+        #     loss[1].item(),
+        #     loss[3].item(),
+        #     loss[2].item()
+        # ))
+        writer.add_scalar('Loss', loss[0].item(), n_iter + epoch * data_sz)
+        writer.add_scalar('Tem Loss', loss[1].item(), n_iter + epoch * data_sz)
+        writer.add_scalar('PemClr Loss', loss[3].item(), n_iter + epoch * data_sz)
+        writer.add_scalar('PemReg Loss', loss[2].item(), n_iter + epoch * data_sz)
 
         epoch_pemreg_loss += loss[2].cpu().detach().numpy()
         epoch_pemclr_loss += loss[3].cpu().detach().numpy()
@@ -44,6 +57,72 @@ def train_BMN(data_loader, model, optimizer, epoch, bm_mask):
             epoch_pemclr_loss / (n_iter + 1),
             epoch_pemreg_loss / (n_iter + 1),
             epoch_loss / (n_iter + 1)))
+
+
+def evaluate(cfg, data_loader, model, epoch, n_iter, writer):
+    model.eval()
+    tscale = 100
+
+    with torch.no_grad():
+        for n_iter, (input_data, label_confidence, label_start, label_end) in enumerate(data_loader):
+            video_name = video_name[0]
+
+            input_data = input_data.cuda()
+            label_start = label_start.cuda()
+            label_end = label_end.cuda()
+            label_confidence = label_confidence.cuda()
+            confidence_map, start, end = model(input_data)
+
+            # print(start.shape,end.shape,confidence_map.shape)
+            start_scores = start_scores[0].cpu().numpy()
+            end_scores = end_scores[0].cpu().numpy()
+
+            max_start = max(start_scores)
+            max_end = max(end_scores)
+
+            #########################################################################
+            # generate the set of start points and end points
+            start_bins = np.zeros(tscale)
+            start_bins[0] = 1  # [1,0,0...,0,1]
+            for idx in range(1, tscale - 1):
+                if start_scores[idx] > start_scores[idx + 1] and start_scores[idx] > start_scores[idx - 1]:
+                    start_bins[idx] = 1
+                elif start_scores[idx] > (0.5 * max_start):
+                    start_bins[idx] = 1
+
+            end_bins = np.zeros(len(end_scores))
+            end_bins[-1] = 1
+            for idx in range(1, tscale - 1):
+                if end_scores[idx] > end_scores[idx + 1] and end_scores[idx] > end_scores[idx - 1]:
+                    end_bins[idx] = 1
+                elif end_scores[idx] > (0.5 * max_end):
+                    end_bins[idx] = 1
+            #########################################################################
+
+            #########################################################################
+            new_props = []
+            for idx in range(tscale):
+                for jdx in range(tscale):
+                    start_index = jdx
+                    end_index = start_index + idx + 1
+                    if end_index < tscale and start_bins[start_index] == 1 and end_bins[end_index] == 1:
+                        xmin = start_index / tscale
+                        xmax = end_index / tscale
+                        xmin_score = start_scores[start_index]
+                        xmax_score = end_scores[end_index]
+                        score = xmin_score * xmax_score
+                        new_props.append([xmin, xmax, xmin_score, xmax_score, score])
+            new_props = np.stack(new_props)
+            #########################################################################
+
+            col_name = ["xmin", "xmax", "xmin_score", "xmax_score", "score"]
+            new_df = pd.DataFrame(new_props, columns=col_name)
+            new_df.to_csv(os.path.join(cfg.BMN.POST_PROCESS.IMD_RESULTS_DIR, video_name + ".csv"), index=False)
+
+    print("Post processing start")
+    BMN_post_processing(cfg)
+    print("Post processing finished")
+    evaluate_proposals(cfg)
 
 
 def test_BMN(data_loader, model, epoch, bm_mask):
@@ -84,7 +163,7 @@ def test_BMN(data_loader, model, epoch, bm_mask):
 
 def BMN_Train(opt):
     model = BMN(opt)
-    model = torch.nn.DataParallel(model, device_ids=[0, 1]).cuda()
+    model = torch.nn.DataParallel(model, device_ids=[0]).cuda()
     optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=opt["training_lr"],
                            weight_decay=opt["weight_decay"])
 
@@ -95,18 +174,20 @@ def BMN_Train(opt):
     test_loader = torch.utils.data.DataLoader(VideoDataSet(opt, subset="validation"),
                                               batch_size=opt["batch_size"], shuffle=False,
                                               num_workers=8, pin_memory=True)
+    
+    writer = SummaryWriter('log/')
 
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=opt["step_size"], gamma=opt["step_gamma"])
     bm_mask = get_mask(opt["temporal_scale"])
     for epoch in range(opt["train_epochs"]):
         scheduler.step()
-        train_BMN(train_loader, model, optimizer, epoch, bm_mask)
+        train_BMN(train_loader, model, optimizer, epoch, bm_mask, writer)
         test_BMN(test_loader, model, epoch, bm_mask)
 
 
 def BMN_inference(opt):
     model = BMN(opt)
-    model = torch.nn.DataParallel(model, device_ids=[0, 1]).cuda()
+    model = torch.nn.DataParallel(model, device_ids=[0]).cuda()
     checkpoint = torch.load(opt["checkpoint_path"] + "/BMN_best.pth.tar")
     model.load_state_dict(checkpoint['state_dict'])
     model.eval()
